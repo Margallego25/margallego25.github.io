3a482bc9037d81de0a2a68fb5be58f53f7dd99a1
Mar Gallego <49495700+Margallego25@users.noreply.github.com>
2020-04-29 18:31:34 +0200

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -22,7 +22,6 @@
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
-  $para = 'tradumar25@gmail.com';
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   /*
